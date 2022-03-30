85a721efc598bb4137a8289fb0b919d15ee36606
Julian Bayer <jdbayer09@hotmail.com>
2022-03-30 10:07:17 -0500

    commit inicial

---
+++ src/main/java/com/jdb/personal/acc/CuentasPersonalesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.jdb.personal.acc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentasPersonalesServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuentasPersonalesServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdb/personal/acc/CuentasPersonalesServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdb.personal.acc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentasPersonalesServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
